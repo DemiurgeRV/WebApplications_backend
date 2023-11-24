7eb69e3d4cbb4ac74c617ecec57b3128a8beb77a
Vladislav Rybin <rybinvladislav@mail.ru>
2023-11-25 01:01:02 +0300

    Made design for 1st page and created some services

--- FiltersForImages/main/urls.py
+++ FiltersForImages/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('home/', views.getFilters),
+    path('filter/<int:id>/', views.getFilter, name='filter_url')
 ]
--- FiltersForImages/main/views.py
+++ FiltersForImages/main/views.py
@@ -1,4 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("<h4>Проверка!!!</h4>")
+def getFilters(request):
+    return render(request, 'main/AllFilters.html', {'data': {
+        'filters': [
+            {'name': 'Обрезка и поворот', 'id': 1, 'image_url': 'main/img/editor-img.png', 'price': '25', 'time': '1-2'},
+            {'name': 'Коррекция изображения', 'id': 2, 'image_url': 'main/img/preset-img.jpg', 'price': '60', 'time': '1-2'},
+            {'name': 'Фоторамки, стикеры и текст', 'id': 3, 'image_url': 'main/img/dog-img-1.jpg', 'price': '35-50', 'time': '2'},
+            {'name': 'Услуга 4', 'id': 4, 'image_url': 'main/img/editor-img.png', 'price': '', 'time': ''},
+        ]
+    }})
+
+def getFilter(request, id):
+    return render(request, 'main/OneFilter.html', {'data': {
+        'id': id
+    }})
