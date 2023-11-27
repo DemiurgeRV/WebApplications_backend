8937d718daa1dd07c80b1ad44196809f7d702c93
Vladislav Rybin <rybinvladislav@mail.ru>
2023-11-27 17:49:56 +0300

    Added some other services

--- FiltersForImages/main/views.py
+++ FiltersForImages/main/views.py
@@ -5,8 +5,9 @@ def getFilters(request):
         'filters': [
             {'name': 'Обрезка и поворот', 'id': 1, 'image_url': 'main/img/editor-img.png', 'price': '25', 'time': '1-2'},
             {'name': 'Коррекция изображения', 'id': 2, 'image_url': 'main/img/preset-img.jpg', 'price': '60', 'time': '1-2'},
-            {'name': 'Фоторамки, стикеры и текст', 'id': 3, 'image_url': 'main/img/dog-img-1.jpg', 'price': '35-50', 'time': '2'},
-            {'name': 'Услуга 4', 'id': 4, 'image_url': 'main/img/editor-img.png', 'price': '', 'time': ''},
+            {'name': 'Фоторамки, стикеры и текст', 'id': 3, 'image_url': 'main/img/dog-img-3.jpg', 'price': '35-50', 'time': '2'},
+            {'name': 'Фотоэффекты и фотофильтры', 'id': 4, 'image_url': 'main/img/effects-img.jpg', 'price': '100', 'time': '2'},
+            {'name': 'Комплексная обработка', 'id': 5, 'image_url': 'main/img/photo-proccesing-img.png', 'price': '600', 'time': '5'},
         ]
     }})
 
