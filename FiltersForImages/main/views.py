a2c8a80cdb450bf47b326510d25e3d57138c65c4
Vladislav Rybin <rybinvladislav@mail.ru>
2023-11-28 00:00:01 +0300

    Added search form

--- FiltersForImages/main/views.py
+++ FiltersForImages/main/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 
-def getFilters(request):
-    return render(request, 'main/AllFilters.html', {'data': {
-        'filters': [
-            {'name': 'Обрезка и поворот', 'id': 1, 'image_url': 'main/img/editor-img.png', 'price': '25', 'time': '1-2'},
-            {'name': 'Коррекция изображения', 'id': 2, 'image_url': 'main/img/preset-img.jpg', 'price': '60', 'time': '1-2'},
-            {'name': 'Фоторамки, стикеры и текст', 'id': 3, 'image_url': 'main/img/dog-img-3.jpg', 'price': '35-50', 'time': '2'},
-            {'name': 'Фотоэффекты и фотофильтры', 'id': 4, 'image_url': 'main/img/effects-img.jpg', 'price': '100', 'time': '2'},
-            {'name': 'Комплексная обработка', 'id': 5, 'image_url': 'main/img/photo-proccesing-img.png', 'price': '600', 'time': '5'},
+data = { 'data': {'filters': [
+            {'name': 'Обрезка и поворот', 'id': 1, 'image_url': 'main/img/editor-img.png',
+             'price': '25', 'time': '1-2'},
+            {'name': 'Коррекция изображения', 'id': 2, 'image_url': 'main/img/preset-img.jpg',
+             'price': '60', 'time': '1-2'},
+            {'name': 'Фоторамки, стикеры и текст', 'id': 3, 'image_url': 'main/img/dog-img-3.jpg',
+             'price': '35-50', 'time': '2'},
+            {'name': 'Фотоэффекты и фотофильтры', 'id': 4, 'image_url': 'main/img/effects-img.jpg',
+             'price': '100', 'time': '2'},
+            {'name': 'Комплексная обработка', 'id': 5, 'image_url': 'main/img/photo-processing-img.png',
+             'price': '600', 'time': '5'},
         ]
-    }})
+    }}
+
+def getFilters(request):
+    info = []
+    input_text = request.GET.get('find')
+    if input_text is not None:
+        for filter in data['data']['filters']:
+            if input_text in filter['name']:
+                info.append(filter)
+        return render(request, 'main/AllFilters.html', { 'data' : { 'filters' : info }})
+
+    else:
+        return render(request, 'main/AllFilters.html', data)
 
 def getFilter(request, id):
     return render(request, 'main/OneFilter.html', {'data': {
