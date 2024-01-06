c9dd1bc4e53b23c67381a20e80f2a1671ab1e9d1
Vladislav Rybin <rybinvladislav@mail.ru>
2024-01-06 17:16:10 +0300

    Added button 'To Home Page'

--- FiltersForImages/main/urls.py
+++ FiltersForImages/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.getFilters),
+    path('home/', views.getFilters, name='toHome'),
     path('filter/<int:id>/', views.getFilter, name='filter_url')
 ]
