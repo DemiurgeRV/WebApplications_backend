c378130454887669e8f6669cc00089f1ebf0df45
Vladislav Rybin <rybinvladislav@mail.ru>
2024-01-07 16:44:43 +0300

    home

--- FiltersForImages/main/urls.py
+++ FiltersForImages/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.getFilters, name='toHome'),
+    path('home/', views.getFilters, name='Home'),
     path('filter/<int:id>/', views.getFilter, name='filter_url')
 ]
