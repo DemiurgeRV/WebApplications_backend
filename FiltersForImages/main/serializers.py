9031f955c1c556aa4b500358a7f28f878ff66dda
Vladislav Rybin <rybinvladislav@mail.ru>
2024-01-31 20:02:19 +0300

    the output user's login in orders

--- FiltersForImages/main/serializers.py
+++ FiltersForImages/main/serializers.py
@@ -6,12 +6,19 @@ class FiltersSerializer(serializers.ModelSerializer):
         model = Filters
         fields = "__all__"
 
-class OrdersSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Orders
-        fields = "__all__"
-
 class FilterOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = FilterOrder
+        fields = "__all__"
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ('login', 'email', 'id')
+
+class OrdersSerializer(serializers.ModelSerializer):
+    owner = UsersSerializer(read_only=True)
+    moderator = UsersSerializer(read_only=True)
+    class Meta:
+        model = Orders
         fields = "__all__"
