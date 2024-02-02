d024a206e1cf9805c230bfb3547ad3b7c0eb74df
Vladislav Rybin <rybinvladislav@mail.ru>
2024-02-02 22:09:07 +0300

    timezone and image's url

--- FiltersForImages/main/urls.py
+++ FiltersForImages/main/urls.py
@@ -3,23 +3,23 @@ from . import views
 
 urlpatterns = [
     path('api/filters/', views.filters_list),
-    path('api/filter/<int:id>/', views.one_filter),
-    path('api/filter/create/', views.create_filter),
-    path('api/filter/<int:id>/update/', views.update_filter),
-    path('api/filter/<int:id>/delete/', views.delete_filter),
-    path('api/filter/<int:id>/add_to_order/', views.add_to_order),
+    path('api/filters/<int:id>/', views.one_filter),
+    path('api/filters/create/', views.create_filter),
+    path('api/filters/<int:id>/update/', views.update_filter),
+    path('api/filters/<int:id>/delete/', views.delete_filter),
+    path('api/filters/<int:id>/add_to_order/', views.add_to_order),
 
     path('api/orders/', views.orders_list),
-    path('api/order/<int:id>/', views.one_order),
-    path('api/order/<int:id>/update/', views.update_order),
-    path('api/order/<int:id>/update_status_owner/', views.update_status_owner),
-    path('api/order/<int:id>/update_status_moderator/', views.update_status_moderator),
-    path('api/order/<int:id>/delete/', views.delete_order),
-    path('api/order/<int:order_id>/delete_filter/<int:filter_id>/', views.delete_filter_from_order),
-    path('api/order/<int:order_id>/update_filter/<int:filter_id>/', views.update_order_filter),
+    path('api/orders/<int:id>/', views.one_order),
+    path('api/orders/<int:id>/update/', views.update_order),
+    path('api/orders/<int:id>/update_status_owner/', views.update_status_owner),
+    path('api/orders/<int:id>/update_status_moderator/', views.update_status_moderator),
+    path('api/orders/<int:id>/delete/', views.delete_order),
+    path('api/orders/<int:order_id>/delete_filter/<int:filter_id>/', views.delete_filter_from_order),
+    path('api/orders/<int:order_id>/update_filter/<int:filter_id>/', views.update_order_filter),
 
-    path('api/filter/<int:id>/image/', views.get_image),
-    path('api/filter/<int:id>/image/update/', views.update_image)
+    path('api/filters/<int:id>/image/', views.get_image),
+    path('api/filters/<int:id>/image/update/', views.update_image)
 ]
 
 
