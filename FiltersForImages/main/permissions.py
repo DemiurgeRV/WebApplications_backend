87d3d997ce6a1da1c5f450df7a7ed8c4f545a369
Vladislav Rybin <rybinvladislav@mail.ru>
2024-02-11 14:10:12 +0300

    Moder permissions for redis

--- FiltersForImages/main/permissions.py
+++ FiltersForImages/main/permissions.py
@@ -1,13 +1,18 @@
 from rest_framework import permissions
 import redis
 from django.conf import settings
+from .models import Users
 
 session_storage = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
 
 class IsModerator(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return request.user.role
+        ssid = request.COOKIES.get("session_id", None)
+        if ssid != None:
+            user_name = session_storage.get(ssid).decode('utf-8')
+            if user_name != None:
+                user_object = Users.objects.get(login=user_name)
+                return user_object.role
         return False
 
 # class IsAdmin(permissions.BasePermission):
